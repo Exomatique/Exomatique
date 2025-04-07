71bc35cac727bd93698bb3cfdbd3a1dc32cc588c
Wyrdix <wyrdix@gmail.com>
2025-04-07 14:16:16 +0200

    👽️ Update editor

--- src/lib/document/Editor.svelte
+++ src/lib/document/Editor.svelte
@@ -1,26 +1,47 @@
 <script lang="ts">
-	import type { ExoData } from '@exomatique/editor';
+	import type { ExoData, SemanticVariant } from '@exomatique/editor';
 	import {
-		DefinitionModule,
 		Editor,
 		ExoEditor,
 		LatexModule,
 		MdModule,
-		TheoremModule,
+		SemanticModule,
 		VideoModule
 	} from '@exomatique/editor';
 
 	let { data = $bindable(), editable = $bindable(false) }: { data: ExoData; editable?: boolean } =
 		$props();
 
+	const simple_variant = (
+		name: string,
+		color: string,
+		title?: boolean,
+		spoiler?: boolean
+	): SemanticVariant => ({
+		name: name,
+		baseColor: { type: 'static', value: color },
+		prefix: { type: 'static', value: name },
+		spoiler: { type: 'static', value: spoiler || false },
+		title: title === false ? { type: 'none' } : { type: 'editable', value: '' }
+	});
+
+	const variants: SemanticVariant[] = [
+		{
+			name: 'Custom Box',
+			baseColor: { type: 'editable', value: '#000000' },
+			prefix: { type: 'editable', value: 'Custom Box' },
+			spoiler: { type: 'editable', value: false },
+			title: { type: 'editable', value: '' }
+		},
+		simple_variant('Definition', '#0000ff'),
+		simple_variant('Theorem', '#ff0000'),
+		simple_variant('Question', '#000000'),
+		simple_variant('Hint', '#cccc00', false, true),
+		simple_variant('Solution', '#00aa00', false, true)
+	];
+
 	const exo_editor = new ExoEditor({
-		modules: [
-			new MdModule(),
-			new DefinitionModule(),
-			new TheoremModule(),
-			new LatexModule(),
-			new VideoModule()
-		],
+		modules: [new MdModule(), new SemanticModule(variants), new LatexModule(), new VideoModule()],
 		default_module: 'md'
 	});
 </script>
